d909981ae100f59cdc98f79420661662269a3099
jermany1717 <leejm0201@gmail.com>
2024-05-29 00:48:02 +0900

    준민 - TETRIS 전체적인 게임 작동 구현

--- tetris.c
+++ tetris.c
@@ -8,9 +8,36 @@
 #include <stdbool.h>
 #include "tetrisheader.h"
 
+int posX = 12, posY = 0;
 int tetris() {
+    speed = 30; // 블록 이동 속도
     srand((unsigned int)time(NULL)); // 난수 생성
     removeCursor();  // 커서 제거
     DrawGameBoard(); // 게임 보드 그리기
-	return 0;
+
+    // 메인 게임 루프
+    while (1) {
+        if (IsGameOver() == 0) { // 게임 오버 확인
+            break;
+        }
+        while (1)
+        {
+            if (BlockDown() == 0) // 블록이 더 이상 내려갈 수 없는 경우
+            {
+                AddBlockToBoard();  // 블록을 게임 보드에 추가
+                RemoveFillUpLine(); // 가득 찬 줄 제거
+                DrawGameBoard(); // 게임 보드 그리기
+                break;
+            }
+            ProcessKeyInput(); // 키 입력 처리
+        }
+        // 새로운 블록 생성 위치
+        posX = 6; posY = 2;
+        block_id = (rand() % 7) * 4; // 새로운 블록 랜덤 생성
+    }
+    // 게임 오버 메시지 출력
+    setCurrentCursorPos(14, 0);
+    puts("GameOver");
+    getchar();
+    return 0;
 }
