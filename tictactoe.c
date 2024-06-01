84c7e3943a0b1795718b922a563fcbe040a542e9
everyday6 <everyday06@sju.ac.kr>
2024-06-01 13:05:45 +0900

    지원 - checkGameState에 있던 오류 수정

--- tictactoe.c
+++ tictactoe.c
@@ -87,6 +87,13 @@ int checkGameState() {
             return WIN;
         }
     }
+
+    for (int i = 0;i < 3;i++) {
+        if (board[i] == board[i + 3] && board[i + 3] == board[i + 6]) {
+            return WIN;
+        }
+    }
+
     if ((board[0] == board[4] && board[4] == board[8]) ||
         (board[2] == board[4] && board[4] == board[6])) {
         return WIN;
