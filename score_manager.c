b936a558bc63e8d47ff99b6924cab50909b2ac5f
username056 <didtnsdl0406@gmail.com>
2024-06-06 23:20:47 +0900

    소영 - score : 파일 존재여부 확인, 초기값 삽입 checkFileExist() 함수 생성

--- score_manager.c
+++ score_manager.c
@@ -1,2 +1,25 @@
 #include "score_manager.h"
 #include <stdio.h>
+
+
+void checkFileExist() {
+
+    FILE* fp;
+    fp = fopen("SOFTWAREARCADE_SCORE.txt", "r+");
+
+    if (fp == NULL) {
+        fp = fopen("SOFTWAREARCADE_SCORE.txt", "w");
+
+        for (int i = 0; i < 10; i++) {          // 스네이크 게임 점수 초기화
+            fputs("SNAKE 000000\n", fp);
+        }
+        for (int i = 0; i < 10; i++) {          // 테트리스 게임 점수 초기화
+            fputs("TETRIS 000000\n", fp);
+        }
+        fputs("TICTACTOE 0 0 0\n", fp);                   // 틱택토 게임 승리/패배/무승부 초기화
+        fclose(fp);
+    }
+    else
+        fclose(fp);
+
+}
--- score_manager.h
+++ score_manager.h
@@ -1,6 +1,6 @@
 #ifndef SCORE_MANAGER_H
 #define SCORE_MANAGER_H
 
-
+void checkFileExist();
 
 #endif 
