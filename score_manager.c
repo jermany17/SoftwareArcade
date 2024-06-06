719f7c562dfe749dde30f419b3d35ea25b2ce04a
username056 <didtnsdl0406@gmail.com>
2024-06-06 23:41:30 +0900

    소영 - score : 파일 변경 필요 여부 확인하는 checkScore 함수 생성

--- score_manager.c
+++ score_manager.c
@@ -1,6 +1,6 @@
 #include "score_manager.h"
 #include <stdio.h>
-
+#include <string.h>
 
 void checkFileExist() {
 
@@ -21,5 +21,28 @@ void checkFileExist() {
     }
     else
         fclose(fp);
+
+}
+
+void checkScore(char gameName[10], int curGameScore) {
+
+    char savedGameName[10];
+    int savedGameScore;
+
+    FILE* file = fopen("game_scores.txt", "r");
+    if (file == NULL) {
+        printf("score file does not exist");
+        exit(-1);
+    }
+
+    while (fscanf(file, "%s %d", savedGameName, &savedGameScore) != EOF) {
+        if (strcmp(gameName, savedGameName) == 0 && curGameScore > savedGameScore) {
+            fclose(file);
+            updateScore(gameName, curGameScore);
+            return;
+        }
+    }
+
+    fclose(file);
 
 }
--- score_manager.h
+++ score_manager.h
@@ -2,5 +2,6 @@
 #define SCORE_MANAGER_H
 
 void checkFileExist();
+void checkScore(char gameName[10], int curGameScore);
 
 #endif 
